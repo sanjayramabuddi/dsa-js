5735539d99e7440774aba721761dd861db269d31
sanjayramabuddi <sanjayramabuddi@gmail.com>
2025-07-16 23:01:29 +0530

    started with linked list

---
+++ S&S-01/selectionSort.js
@@ -0,0 +1,21 @@
+let arr = [5, 9, 2, 4, 7, 3, 1];
+
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort(arr));
